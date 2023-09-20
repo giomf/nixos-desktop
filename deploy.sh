da0b5c4c6949c048ae467fe007befc05970e6013
Guillaume Fournier <me@guif.dev>
2023-09-20 22:16:36 +0200

    rename to rebuild

--- deploy.sh
+++
@@ -1,1 +0,0 @@
-sudo cp * /etc/nixos/ && sudo nixos-rebuild switch
---
+++ rebuild.sh
@@ -0,0 +1,21 @@
+#!/run/current-system/sw/bin/bash -e
+
+if [[ -z "$1" || "$1" == "help" ]]; then
+    script_name="$(basename "$0")"
+    echo -e "Control script for nixos-rebuild\n"
+    echo -e "Usage: ${script_name} <command>\n"
+    echo "Commands:"
+    echo -e "\tswitch"
+    echo -e "\tboot"
+
+elif [[ "$1" == "switch" ]]; then
+    sudo cp * /etc/nixos/ && sudo nixos-rebuild switch
+elif [[ "$1" == "boot" ]]; then
+    sudo cp * /etc/nixos/ && sudo nixos-rebuild boot
+else 
+    echo "ERROR: Command $1 not found. Aborting!"
+    echo "Use help"
+    exit 1
+fi
+
+exit 0
