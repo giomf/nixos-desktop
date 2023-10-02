dc33a0f5ade95790b5c8d3c94e337b65aa8b7c7c
Guillaume Fournier <me@guif.dev>
2023-10-02 10:09:37 +0200

    Allow 10 generations

--- nixos/default.nix
+++ nixos/default.nix
@@ -11,6 +11,7 @@
 
 	# Bootloader
 	boot.loader.systemd-boot.enable = true;
+	boot.loader.systemd-boot.configurationLimit = 10;
 	boot.loader.efi.canTouchEfiVariables = true;
 
 	time.timeZone = "Europe/Berlin";
