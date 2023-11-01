f4f4f8fb90672c79cce21f0ea116a2acee05fe5c
Guillaume Fournier <me@guif.dev>
2023-11-01 16:40:15 +0100

    add gh-cli add nixpkgs-review

--- home/default.nix
+++ home/default.nix
@@ -1,42 +1,42 @@
 { config, pkgs, ... }:
 
 {
-    imports = [
-        ./fish.nix
-        ./joshuto.nix
-        ./helix.nix
-        ./starship.nix
-    ];
+  imports = [
+    ./fish.nix
+    ./joshuto.nix
+    ./helix.nix
+    ./starship.nix
+  ];
 
-    # Home Manager needs a bit of information about you and the
-    # paths it should manage.
-    home = {
-        username = "giom";
-        homeDirectory = "/home/giom";
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home = {
+    username = "giom";
+    homeDirectory = "/home/giom";
 
-        # This value determines the Home Manager release that your
-        # configuration is compatible with. This helps avoid breakage
-        # when a new Home Manager release introduces backwards
-        # incompatible changes.
-        #
-        # You can update Home Manager without changing this value. See
-        # the Home Manager release notes for a list of state version
-        # changes in each release.
-        stateVersion = "23.05";
-    };
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "23.05";
+  };
 
-    # Let Home Manager install and manage itself.
-    programs.home-manager.enable = true;
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 
 
-	# Packages
-	home.packages = with pkgs; [
-		# Base
-		eza
-		bat
-		flameshot
-		btop
-		unzip
+  # Packages
+  home.packages = with pkgs; [
+    # Base
+    eza
+    bat
+    flameshot
+    btop
+    unzip
     zellij
     wget
     docker-compose
@@ -46,10 +46,12 @@
     file
     kmon
 
-		# Coding
-		git
-	];
+    # Coding
+    git
+    nixpkgs-review
+    gh
+  ];
 
-	# Services
-	services.ssh-agent.enable = true;
+  # Services
+  services.ssh-agent.enable = true;
 }
