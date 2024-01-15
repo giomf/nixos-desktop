6524df131e275d3d548cd8f53d69960fc7450fa0
Guillaume Fournier <me@guif.dev>
2024-01-15 16:39:19 +0100

    Ad helix config

--- home/helix.nix
+++ home/helix.nix
@@ -1,11 +1,23 @@
 { config, pkgs, ... }:
 
 {
+
+  home.packages = with pkgs; [
+    nil
+    nodePackages_latest.bash-language-server
+    python311Packages.python-lsp-server
+  ];
+  
   programs.helix = {
     enable = true;
     defaultEditor = true;
     settings = {
       theme = "github_dark_dimmed";
+      editor = {
+        line-number = "relative";
+        rulers = [120];
+        mouse = false;
+      };
     };
   };
 }
