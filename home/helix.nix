2ce9472c95a50aa551bda0fe3cc3f52a4e482e6f
Guillaume Fournier <me@guif.dev>
2024-01-16 15:48:35 +0100

    Add nix formatter

--- home/helix.nix
+++ home/helix.nix
@@ -3,12 +3,17 @@
 {
 
   home.packages = with pkgs; [
+    # nix
     nil
+    nixfmt
+    # bash
     nodePackages_latest.bash-language-server
+    # python
     python311Packages.python-lsp-server
+    # rust
     rust-analyzer
   ];
-  
+
   programs.helix = {
     enable = true;
     defaultEditor = true;
@@ -16,17 +21,24 @@
       theme = "github_dark_dimmed";
       editor = {
         line-number = "relative";
-        rulers = [120];
+        rulers = [ 120 ];
         mouse = false;
         statusline = {
-          left = ["mode" "spinner" "version-control"];
-          center = ["file-base-name" "file-modification-indicator" "diagnostics"];
-          right = ["selections" "position" "total-line-numbers"];
+          left = [ "mode" "spinner" "version-control" ];
+          center = [ "file-base-name" "file-modification-indicator" "diagnostics" ];
+          right = [ "selections" "position" "total-line-numbers" ];
         };
-        lsp = {
-          display-messages = true;
-        };
+        lsp = { display-messages = true; };
       };
+    };
+    languages = {
+      language = [{
+        name = "nix";
+        formatter = {
+          command = "nixfmt";
+          args = [ "-" "-w" "120" "-q" ];
+        };
+      }];
     };
   };
 }
