f26fcb9254ac56e5f82cacbc69499068d7084f9d
Guillaume Fournier <me@guif.dev>
2024-02-11 13:20:38 +0100

    edit helix config

--- home/helix.nix
+++ home/helix.nix
@@ -24,12 +24,38 @@
         rulers = [ 120 ];
         mouse = false;
         color-modes = true;
+        cursor-shape = {
+          normal = "block";
+          insert = "bar";
+          select = "underline";
+        };
         statusline = {
           left = [ "mode" "spacer" "spinner" "spacer" "version-control" ];
           center = [ "file-base-name" "file-modification-indicator" "diagnostics" ];
           right = [ "selections" "position" "total-line-numbers" ];
         };
         lsp = { display-messages = true; };
+      };
+
+      keys = {
+        normal = {
+          A-j = [ "extend_to_line_bounds" "delete_selection" "move_line_down" "paste_before" ];
+          A-k = [ "extend_to_line_bounds" "delete_selection" "move_line_up" "paste_before" ];
+          C-j = [ "extend_to_line_bounds" "yank" "move_line_down" "paste_before" ];
+          C-k = [ "extend_to_line_bounds" "yank" "move_line_up" "paste_after" ];
+          D = "delete_char_backward";
+          e = "move_next_word_end";
+          H = "goto_line_start";
+          J = [ "move_visual_line_down" "move_visual_line_down" "move_visual_line_down" ];
+          K = [ "move_visual_line_up" "move_visual_line_up" "move_visual_line_up" ];
+          L = "goto_line_end";
+          w = "move_prev_word_start";
+          W = "move_prev_long_word_start";
+          X = "extend_line_above";
+        };
+        insert = {
+          j = { k = "normal_mode"; };
+        };
       };
     };
     languages = {
