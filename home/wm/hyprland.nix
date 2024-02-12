9bf61c57ad1d9cd293ac17a8b4ddedb81e9a43b1
Guillaume Fournier <me@guif.dev>
2024-02-12 12:06:31 +0100

    introduce hjkl movement

--- home/wm/hyprland.nix
+++ home/wm/hyprland.nix
@@ -25,21 +25,21 @@
         			exec-once = nm-applet --indicator
         			exec-once = nextcloud --background
         			exec-once = swaybg --mode fill --image ${./wallpaper.png}
-              exec-once = [workspace 1] telegram-desktop
-              exec-once = [workspace 1] element-desktop
-              exec-once = [workspace 5] thunderbird 
-              exec-once = [workspace 9] keepassxc 
+#              exec-once = [workspace 1] telegram-desktop
+#              exec-once = [workspace 1] element-desktop
+#              exec-once = [workspace 5] thunderbird 
+#              exec-once = [workspace 9] keepassxc 
 
         			bind = $mod, return, exec, $term -e zellij attach -c TERMINAL
         			bind = $mod, space, exec, $menu
         			bind = $mod SHIFT, return, exec, firefox
         			bind = $mod, F, exec, $term -e ranger
-        			bind = $mod, K, exec, keepassxc
+        			bind = $mod, O, exec, keepassxc
         			bind = $mod, M, exec, thunderbird
         			bind = $mod, T, exec, $term -e btop
-        			bind = $mod, L, exec, grimshot copy area
+        			bind = $mod, I, exec, grimshot copy area
         			bind = $mod, C, exec, $term -e numbat
-        			bind = $mod, J, exec, bash -c if pgrep gammastep; then pkill gammastep; else gammastep -O 4500; fi
+        			bind = $mod, U, exec, bash -c if pgrep gammastep; then pkill gammastep; else gammastep -O 4500; fi
         			bind = $mod, escape, exec, ${pkgs.swaylock-effects}/bin/swaylock -f -S --clock --effect-blur 10x10 --fade-in 1
 
         			binde = , XF86AudioMute, exec, pactl set-sink-mute @DEFAULT_SINK@ toggle
@@ -50,24 +50,16 @@
 
 
         			# move focus
-        			bind = $mod, left, movefocus, l
-        			bind = $mod, right, movefocus, r
-        			bind = $mod, up, movefocus, u
-        			bind = $mod, down, movefocus, d
-        			bind = $mod, A, movefocus, l
-        			bind = $mod, D, movefocus, r
-        			bind = $mod, W, movefocus, u
-        			bind = $mod, S, movefocus, d
+        			bind = $mod, H, movefocus, l
+        			bind = $mod, L, movefocus, r
+        			bind = $mod, K, movefocus, u
+        			bind = $mod, J, movefocus, d
 
         			# move window
-        			bind = $mod SHIFT, left, movewindow, l
-        			bind = $mod SHIFT, right, movewindow, r
-        			bind = $mod SHIFT, up, movewindow, u
-        			bind = $mod SHIFT, down, movewindow, d
-        			bind = $mod SHIFT, A, movewindow, l
-        			bind = $mod SHIFT, D, movewindow, r
-        			bind = $mod SHIFT, W, movewindow, u
-        			bind = $mod SHIFT, S, movewindow, d
+        			bind = $mod SHIFT, H, movewindow, l
+        			bind = $mod SHIFT, L, movewindow, r
+        			bind = $mod SHIFT, K, movewindow, u
+        			bind = $mod SHIFT, J, movewindow, d
 
         			bind = $mod, Q, killactive,
         			bind = $mod, E, fullscreen, 1
