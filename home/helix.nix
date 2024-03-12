7ae079275f29534b4d809225ce864ee22a1064a9
Guillaume Fournier <me@guif.dev>
2024-03-12 12:07:41 +0100

    Add python formatter

--- home/helix.nix
+++ home/helix.nix
@@ -97,6 +97,11 @@
           formatter = { command = "rustfmt"; };
           auto-format = true;
         }
+        {
+          name = "python";
+          formatter = { command = "yapf"; args = [ "--style={based_on_style: google, column_limit: 120, indent_width: 4}" ]; };
+          auto-format = true;
+        }
       ];
     };
   };
