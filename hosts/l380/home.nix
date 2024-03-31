ecd633d7d182627575ada138508d5e8e2379269e
Guillaume Fournier <me@guif.dev>
2024-03-31 19:06:51 +0200

    format code

--- hosts/l380/home.nix
+++ hosts/l380/home.nix
@@ -1,13 +1,16 @@
-{ config, pkgs,  ... }:
+{ config, pkgs, ... }:
 
 {
-  imports = [
-    ../../home
-    ../../home/wm
-  ];
+  imports = [ ../../home ../../home/wm ];
 
   # Packages
-  home.packages = with pkgs; [
-    usbutils
-  ];
+  home.packages = with pkgs; [ usbutils ];
+
+  # workaround for virt-manager
+  # https://discourse.nixos.org/t/virt-manager-cannot-create-vm/38894/4
+  home.pointerCursor = {
+    gtk.enable = true;
+    package = pkgs.vanilla-dmz;
+    name = "Vanilla-DMZ";
+  };
 }
