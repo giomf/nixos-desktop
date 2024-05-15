bfb6d1f4611c22e4dfb8a593f8513517f749e42a
Guillaume Fournier <me@guif.dev>
2024-05-15 12:50:50 +0200

    Add git alias

--- home/fish.nix
+++ home/fish.nix
@@ -49,6 +49,7 @@
       "gca" = "git commit -a";
       "gch" = "git checkout";
       "gdf" = "git diff";
+      "gfp" = "git fetch -ap";
       "glg" = "git log --decorate --graph --pretty=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%ad%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''            %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%n' --date=local --all";
       "gph" = "git push";
       "gpr" = "git pull --rebase";
