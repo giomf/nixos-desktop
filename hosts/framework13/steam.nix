a52d882beb3d443de1bbc1ba674be04032578bd0
Guillaume Fournier <me@guif.dev>
2025-02-21 12:49:38 +0100

    Deactivate bluetooth tweaks

--- hosts/framework13/steam.nix
+++ hosts/framework13/steam.nix
@@ -10,16 +10,16 @@
 
   hardware = {
     steam-hardware.enable = true;
-    bluetooth = {
-      settings.General = {
-        # https://www.reddit.com/r/NixOS/comments/1ch5d2p/comment/lkbabax/
-        # for pairing bluetooth controller
-        Privacy = "device";
-        JustWorksRepairing = "always";
-        Class = "0x000100";
-        FastConnectable = true;
-      };
-    };
-    xpadneo.enable = true;
+    # bluetooth = {
+    #   settings.General = {
+    #     # https://www.reddit.com/r/NixOS/comments/1ch5d2p/comment/lkbabax/
+    #     # for pairing bluetooth controller
+    #     Privacy = "device";
+    #     JustWorksRepairing = "always";
+    #     Class = "0x000100";
+    #     FastConnectable = true;
+    #   };
+    # };
+    # xpadneo.enable = true;
   };
 }
