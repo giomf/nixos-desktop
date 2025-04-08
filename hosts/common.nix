53a3a851ed00520a0e2d7cb9e96fdef0052bd704
Guillaume Fournier <me@guif.dev>
2025-04-08 21:32:05 +0200

    Remove substituters since its not needed anymore

--- hosts/common.nix
+++ hosts/common.nix
@@ -5,10 +5,10 @@
     ../common/desktop.nix
   ];
 
-  nix.settings = {
-    substituters = [ "https://nix-community.cachix.org" ];
-    trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
-  };
+  # nix.settings = {
+  #   substituters = [ "https://nix-community.cachix.org" ];
+  #   trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+  # };
 
   security = {
     polkit.enable = true;
