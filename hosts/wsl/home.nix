77358b10ff241ecf230a6286a7f3eb254c49a40d
Guillaume Fournier <me@guif.dev>
2025-05-06 11:57:07 +0200

    Remove project related stuff

--- hosts/wsl/home.nix
+++ hosts/wsl/home.nix
@@ -6,17 +6,16 @@
     # ../../home/keyring.nix
   ];
 
-  home.packages = [
-    (pkgs.python313.withPackages (ppkgs: [
-      ppkgs.jedi-language-server
-      ppkgs.mypy
-      ppkgs.pylint
-      ppkgs.python-lsp-ruff
-      ppkgs.python-lsp-server
-      ppkgs.ruff
-      ppkgs.yapf
-    ]))
-  ];
+  # home.packages = [
+  #   (pkgs.python313.withPackages (ppkgs: [
+  #     ppkgs.jedi-language-server
+  #     ppkgs.mypy
+  #     ppkgs.pylint
+  #     ppkgs.python-lsp-ruff
+  #     ppkgs.python-lsp-server
+  #     ppkgs.ruff
+  #     ppkgs.yapf
+  #   ]))
+  # ];
 
-  xdg.configFile."zellij/layouts/musmol.kdl".source = ./musmol.kdl;
 }
