95c7305c58fc89fecb7895e1142e3511a620b20a
Guillaume Fournier <me@guif.dev>
2025-06-01 17:28:08 +0200

    Add alacritty as terminal emulator

--- hosts/framework13/cosmic.nix
+++ hosts/framework13/cosmic.nix
@@ -4,5 +4,11 @@
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
   services.gnome.gnome-keyring.enable = false;
+  # environment.cosmic.excludePackages = with pkgs; [
+  #   cosmic-player
+  #   cosmic-term
+  #   cosmic-wallpapers
+  #   cosmic-edit
+  # ];
 
 }
