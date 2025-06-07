125495ac0beca9f675a740b60e11c64d1cf50e6c
Guillaume Fournier <me@guif.dev>
2025-06-07 17:56:15 +0200

    Update system

--- hosts/framework13/cosmic.nix
+++ hosts/framework13/cosmic.nix
@@ -4,11 +4,10 @@
   services.desktopManager.cosmic.enable = true;
   services.displayManager.cosmic-greeter.enable = true;
   services.gnome.gnome-keyring.enable = false;
-  # environment.cosmic.excludePackages = with pkgs; [
-  #   cosmic-player
-  #   cosmic-term
-  #   cosmic-wallpapers
-  #   cosmic-edit
-  # ];
-
+  environment.cosmic.excludePackages = with pkgs; [
+    cosmic-player
+    cosmic-term
+    cosmic-wallpapers
+    cosmic-edit
+  ];
 }
