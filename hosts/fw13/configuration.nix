adbfdef90d640f64f45900ee8d1e86dbe67db5a4
Guillaume Fournier <me@guif.dev>
2026-03-05 17:03:04 +0100

    Add copilot

--- hosts/fw13/configuration.nix
+++ hosts/fw13/configuration.nix
@@ -6,25 +6,31 @@ let
   hostName = "glap";
 in
 {
-  flake.modules.nixos.${hostName} = {
-    imports = with inputs.self.modules.nixos; [
-      guif
-      system-laptop
-      systemd-boot
-    ];
-
-    home-manager.users.guif = {
-      imports = with inputs.self.modules.homeManager; [
+  flake.modules.nixos.${hostName} =
+    { pkgs, ... }:
+    {
+      imports = with inputs.self.modules.nixos; [
+        guif
         system-laptop
+        systemd-boot
       ];
-      xdg.enable = true;
-      xdg.configFile."autostart/Keepassxc.desktop".source = ./autostart/Keepassxc.desktop;
-    };
 
-    system.stateVersion = "24.11";
+      home-manager.users.guif = {
+        imports = with inputs.self.modules.homeManager; [
+          system-laptop
+        ];
+        xdg.enable = true;
+        xdg.configFile."autostart/Keepassxc.desktop".source = ./autostart/Keepassxc.desktop;
 
-    networking = {
-      inherit hostName;
+        home.packages = [
+          pkgs.github-copilot-cli
+        ];
+      };
+
+      system.stateVersion = "24.11";
+
+      networking = {
+        inherit hostName;
+      };
     };
-  };
 }
--- hosts/l14/configuration.nix
+++ hosts/l14/configuration.nix
@@ -6,27 +6,32 @@ let
   hostName = "gwork";
 in
 {
-  flake.modules.nixos.${hostName} = {
-    imports = with inputs.self.modules.nixos; [
-      system-laptop
-
-      lobaro
-      guif
-      systemd-boot
-    ];
-
-    home-manager.users.guif = {
-      imports = with inputs.self.modules.homeManager; [
+  flake.modules.nixos.${hostName} =
+    { pkgs, ... }:
+    {
+      imports = with inputs.self.modules.nixos; [
         system-laptop
+
+        lobaro
+        guif
+        systemd-boot
       ];
-      xdg.enable = true;
-      xdg.configFile."autostart/Keepassxc.desktop".source = ./autostart/Keepassxc.desktop;
-    };
 
-    system.stateVersion = "24.11";
+      home-manager.users.guif = {
+        imports = with inputs.self.modules.homeManager; [
+          system-laptop
+        ];
+        xdg.enable = true;
+        xdg.configFile."autostart/Keepassxc.desktop".source = ./autostart/Keepassxc.desktop;
+        home.packages = [
+          pkgs.github-copilot-cli
+        ];
+      };
 
-    networking = {
-      inherit hostName;
+      system.stateVersion = "24.11";
+
+      networking = {
+        inherit hostName;
+      };
     };
-  };
 }
