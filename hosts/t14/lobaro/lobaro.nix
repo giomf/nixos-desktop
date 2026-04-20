1858629b05eebb1c3b7cae7bbdb29271482358c1
Guillaume Fournier <me@guif.dev>
2026-04-20 16:32:02 +0200

    feat: add udev rules for jlink

--- hosts/t14/lobaro/lobaro.nix
+++ hosts/t14/lobaro/lobaro.nix
@@ -3,20 +3,33 @@
   ...
 }:
 {
-  flake.modules.nixos.lobaro = {
-    systemd.user.targets.lobaro = {
-      description = "Lobaro tool suite";
-      wants = [
-        "lobaro-tool.service"
-        "power-profiler-kit.service"
+  flake.modules.nixos.lobaro =
+    { pkgs, ... }:
+    {
+      systemd.user.targets.lobaro = {
+        description = "Lobaro tool suite";
+        wants = [
+          "lobaro-tool.service"
+          "power-profiler-kit.service"
+        ];
+      };
+
+      # Needed for jlink
+      nixpkgs.config.allowUnfree = true;
+      nixpkgs.config.segger-jlink.acceptLicense = true;
+      nixpkgs.config.permittedInsecurePackages = [
+        "segger-jlink-qt4-874"
       ];
+      services.udev.packages = with pkgs; [
+        segger-jlink
+        nrf-udev
+      ];
+
+      imports = with inputs.self.modules.nixos; [
+        crepo
+        lobaro-tool
+        power-profiler-kit
+      ];
+
     };
-
-    imports = with inputs.self.modules.nixos; [
-      crepo
-      lobaro-tool
-      power-profiler-kit
-    ];
-
-  };
 }
