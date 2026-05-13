dec44fa69f91dc30f6e8a21f4b8cb234196b727a
Guillaume Fournier <me@guif.dev>
2026-05-13 12:33:37 +0200

    feat: update crepo to v0.5.0

--- hosts/t14/lobaro/crepo.nix
+++ hosts/t14/lobaro/crepo.nix
@@ -5,7 +5,7 @@
   flake.modules.nixos.crepo =
     { pkgs, ... }:
     let
-      version = "0.4.2";
+      version = "0.5.0";
       crepo = pkgs.python3Packages.buildPythonPackage {
         pname = "crepo";
         inherit version;
@@ -13,7 +13,7 @@
 
         src = builtins.fetchGit {
           url = "git@github.com:lobaro/crepo.git";
-          rev = "85e369095adf4cfc1f328a23bd6d951ef87ca78b";
+          rev = "82d222956ee319578fe522a6fd2c387b6706083d";
           ref = "refs/tags/${version}";
         };
 
