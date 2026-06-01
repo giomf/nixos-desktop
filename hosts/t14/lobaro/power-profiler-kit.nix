e8fc901710b6dea4cd609cabc4c00d79cffd8555
Guillaume Fournier <me@guif.dev>
2026-06-01 15:10:45 +0200

    feat: Update power profile kit to v1.0.0

--- hosts/t14/lobaro/power-profiler-kit.nix
+++ hosts/t14/lobaro/power-profiler-kit.nix
@@ -5,7 +5,7 @@
   flake.modules.nixos.power-profiler-kit =
     { pkgs, ... }:
     let
-      version = "0.0.3";
+      version = "1.0.0";
       power-profiler-kit = pkgs.python3Packages.buildPythonPackage {
         pname = "power-profiler-kit";
         inherit version;
@@ -13,7 +13,7 @@
 
         src = builtins.fetchGit {
           url = "git@github.com:lobaro/python-lob-ppk.git";
-          rev = "90882c77170ca5fb4df26e218c4c22030e9ec73d";
+          rev = "13796f2c5604d5de100d889e1b83cd4adff50515";
           ref = "refs/tags/${version}";
         };
 
@@ -39,7 +39,7 @@
         partOf = [ "lobaro.target" ];
         serviceConfig = {
           Type = "simple";
-          ExecStart = "${power-profiler-kit}/bin/lob-ppk-tool -p /dev/ttyACM0";
+          ExecStart = "${power-profiler-kit}/bin/lob-ppk-tool";
           Restart = "on-failure";
           RestartSec = "5s";
         };
